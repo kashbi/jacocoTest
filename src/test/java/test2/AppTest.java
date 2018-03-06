802ad64204485ecf1058af9ad07d22cf3fb57194
Sami Kashbi <sami.kashbi@hpe.com>
2018-03-06 11:40:02 +0200

    Another test cluster

---
+++ src/test/java/test2/AppTest.java
@@ -0,0 +1,35 @@
+package test2;
+
+
+import com.hpe.jacoco.test.App;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void anotherTest() {
+
+		throw new IllegalStateException("unique error message unique error message unique error message unique error messageunique error message");
+
+
+	}
+
+//	@Test
+//	public void testLengthOfTheUniqueKey2() {
+//
+//		App obj = new App();
+//		Assert.assertEquals(37, obj.generateUniqueKey().length());
+//		obj.dummyFunction();
+//
+//	}
+//
+//	@Test
+//	public void testLengthOfTheUniqueKey3() {
+//
+//		App obj = new App();
+//		Assert.assertEquals(37, obj.generateUniqueKey().length());
+//		obj.dummyFunction();
+//
+//	}
+}
