108316b616ffe797e2b00623331904a98e6e90ca
kashbi <kashbi@gmail.com>
2019-01-10 13:31:16 +0200

    US #1014 The turn signal is green
    US #1014 The turn signal is green

--- src/test/java/com/hpe/jacoco/test/AppTest.java
+++ src/test/java/com/hpe/jacoco/test/AppTest.java
@@ -6,7 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class AppTest {
-		//comment12345678911s99ssssdasdddsssssssasdasdasdasdasdasdasasd
+		//comment12345678911s99ssssdasdddsssssssasdasdasdasdasdasdasasdsdfsdf
 	@Test
 	public void testLengthOfTheUniqueKey() {
 
